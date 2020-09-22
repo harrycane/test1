f2d776c1bbdd85063cbc33651a259937fa41df23
Oleg Andreev <mrtwista@yandex.ru>
2020-09-22 20:55:40 +0100

    other files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC } from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import { Home } from "./pages/home/home";
+import { Data } from "./pages/data/data";
+import { Performance } from "./pages/performance/performance";
+import css from "./App.module.css";
 
-function App() {
+export const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={css.app}>
+      <nav className={css.appNav}>
+        <ul className={css.appNavList}>
+          <li className={css.appNavListItem}>
+            <Link to="/">Home</Link>
+          </li>
+          <li className={css.appNavListItem}>
+            <Link to="/data">Data</Link>
+          </li>
+          <li className={css.appNavListItem}>
+            <Link to="/performance">Performance</Link>
+          </li>
+        </ul>
+      </nav>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/data">
+          <Data />
+        </Route>
+        <Route path="/performance">
+          <Performance />
+        </Route>
+      </Switch>
     </div>
   );
-}
+};
 
 export default App;
